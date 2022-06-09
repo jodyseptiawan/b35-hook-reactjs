2d55dbaac7e82095ed3124e879c5577ba504b57c
Jedeye <jody.septiawan5@gmail.com>
2022-06-09 10:55:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,83 +1,33 @@
 //import useState & useEffect here
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import { 
     Container, 
-    Row, Col, Form, 
-    Button } from 'react-bootstrap'
+    Row, Col, Card } from 'react-bootstrap'
 
-import Welcome from './Welcome'
-import GuestGreeting from './GuestGreeting'
+
+import dataProduct from './dummyData/product'
 
 function App() {
+    const [datas] = useState(dataProduct)
 
-    const [state, setState] = useState({
-        isLogin: false,
-        user: {
-            email: '',
-            password: ''
-        }
-    })
-
-    // Create DidMount with useEffect inside it can print "App Component Did Mount" & state value here
-    useEffect(() => {
-        console.log("App Component Did Mount")
-    },[])
-
-    // Create DidUpdate with useEffect inside it can print "App Component Did Update" & state value here
-     useEffect(() => {
-        console.log("App Component Did Update")
-    },[state])
-
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-        const email = document.getElementById('email').value
-        const password = document.getElementById('password').value
-        setState({
-            isLogin: true,
-            user: { 
-                email,
-                password
-            }
-        })
-    }
 
     return (
-        <>
-            {state.isLogin ? <Welcome /> : 
-                (<>
-                    <GuestGreeting />
-                    <Container>
-                        <Row 
-                        className="d-flex justify-content-center mt-5">
-                            <Col md="4">
-                            <Form onSubmit={handleOnSubmit}>
-                                <div className="text-center h5">Login</div>
-
-                                <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control 
-                                    id="email"
-                                    name="email" size="sm" type="email" 
-                                    placeholder="Enter email" />
-                                </Form.Group>
-
-                                <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control 
-                                    id="password"
-                                    name="password" size="sm" type="password" 
-                                    placeholder="Password" />
-                                </Form.Group>
-
-                                <Button variant="primary" type="submit" size="sm">
-                                    Submit
-                                </Button>
-                            </Form>
-                            </Col>
-                        </Row>
-                    </Container>
-                </>) }
-        </>
+        <Container>
+            <Row>
+                {datas.map((data,idx)=>(
+                    <Col sm="3" key={idx}>
+                        <Card className="shadow mt-3">
+                            <Card.Body>
+                                <Card.Title>{data.title}</Card.Title>
+                                <p>Price: <b>{data.price}</b></p>
+                                <p>Qty: <b>{data.qty}</b></p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+            
+        </Container>
     )
 }
 
---
+++ src/dummyData/product.js
@@ -0,0 +1,30 @@
+const data = [
+    {
+        title: 'Tas Hitam',
+        img: 'urlll',
+        price: 80000,
+        qty: 73
+    },
+    {
+        title: 'Sepatu Putih',
+        price: 120000,
+        qty: 50
+    },
+    {
+        title: 'Super Car',
+        price: 1200000000,
+        qty: 13
+    },
+    {
+        title: 'Baju Formal',
+        price: 500000,
+        qty: 8
+    },
+    {
+        title: 'Celana Pendek',
+        price: 50000,
+        qty: 152
+    }
+]
+
+export default data
